dc7a1a1879cfa2ebf3d3711226dd8ff4dfd648d9
Tassilo Horn <tsdh@gnu.org>
2015-04-22 22:02:10 +0200

    up

--- src/ttc15_model_execution_funnyqt/core.clj
+++ src/ttc15_model_execution_funnyqt/core.clj
@@ -2,7 +2,8 @@
   (:require [funnyqt.generic :refer [adj adjs]]
             [funnyqt.emf :refer :all]
             [funnyqt.query :refer [forall? exists? the]]
-            [funnyqt.polyfns :as pf]))
+            [funnyqt.polyfns :as pf]
+            [funnyqt.utils :refer [doseq+ mapc]]))
 
 ;;* Load metamodel and generate accessors
 
@@ -14,15 +15,15 @@
 ;;* Solution
 
 (defn init-variables [activity input]
-  (doseq [lv (a/->locals activity)]
+  (doseq+ [lv (a/->locals activity)]
     (when-let [init-value (a/->initialValue lv)]
       (a/->set-currentValue! lv init-value)))
-  (doseq [iv (and input (a/->inputValues input))]
+  (doseq+ [iv (and input (a/->inputValues input))]
     (when-let [val (a/->value iv)]
       (a/->set-currentValue! (a/->variable iv) val))))
 
 (defn set-nodes-running [activity val]
-  (doseq [node (a/->nodes activity)]
+  (doseq+ [node (a/->nodes activity)]
     (a/set-running! node val)))
 
 (defn init-activity [activity]
@@ -39,7 +40,7 @@
 
 (defn offer-one-ctrl-token [node]
   (let [ctrl-t (a/create-ControlToken! nil)]
-    (doseq [out-cf (a/->outgoing node)]
+    (doseq+ [out-cf (a/->outgoing node)]
       (let [offer  (a/create-Offer! nil {:offeredTokens [ctrl-t]})]
         (a/->add-heldTokens! node ctrl-t)
         (a/->add-offers! out-cf offer)))))
@@ -53,9 +54,9 @@
   (let [offers     (mapcat a/->offers (a/->incoming node))
         tokens     (mapcat a/->offeredTokens offers)
         [ctrl-toks fork-toks] ((juxt filter remove) a/isa-ControlToken? tokens)]
-    (doseq [o offers] (edelete! o))
-    (doseq [c ctrl-toks] (a/->set-holder! c nil))
-    (doseq [ft fork-toks]
+    (mapc edelete! offers)
+    (doseq+ [c ctrl-toks] (a/->set-holder! c nil))
+    (doseq+ [ft fork-toks]
       (a/->set-holder! (a/->baseToken ft) nil)
       (a/set-remainingOffersCount! ft (dec (a/remainingOffersCount ft)))
       (when (zero? (a/remainingOffersCount ft))
@@ -88,15 +89,14 @@
   ([n out-cf]
    (let [in-toks (consume-offers n)]
      (a/->set-heldTokens! n in-toks)
-     (doseq [out-cf (if out-cf [out-cf] (a/->outgoing n))]
+     (doseq+ [out-cf (if out-cf [out-cf] (a/->outgoing n))]
        (a/->add-offers!
         out-cf (a/create-Offer!
                 nil {:offeredTokens in-toks}))))))
 
 (pf/defpolyfn exec-node OpaqueAction [oa]
   (consume-offers oa)
-  (doseq [exp (a/->expressions oa)]
-    (eval-exp exp))
+  (mapc eval-exp (a/->expressions oa))
   (offer-one-ctrl-token oa))
 
 (pf/defpolyfn exec-node ActivityFinalNode [afn]
@@ -111,7 +111,7 @@
                               :holder fn
                               :remainingOffersCount (count out-cfs)})
                        in-toks)]
-    (doseq [out-cf out-cfs]
+    (doseq+ [out-cf out-cfs]
       (a/->add-offers! out-cf (a/create-Offer!
                                nil {:offeredTokens out-toks})))))
 
